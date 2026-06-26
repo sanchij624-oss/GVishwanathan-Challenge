4e7405716368dcca3eed65be2c69951bbc7abcbf
sanchitajainn <sanchij624@gmail.com>
2026-06-26 21:50:46 +0530

    Add files via upload

---
+++ day13.cpp
@@ -0,0 +1,60 @@
+//leetcode 29
+//dicide two integers
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+
+        long long dvd = abs((long long)dividend);
+        long long dvs = abs((long long)divisor);
+        long long ans = 0;
+
+        while (dvd >= dvs) {
+            long long temp = dvs;
+            long long multiple = 1;
+
+            while ((temp << 1) <= dvd) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            dvd -= temp;
+            ans += multiple;
+        }
+
+        if ((dividend < 0) ^ (divisor < 0))
+            ans = -ans;
+
+        return (int)ans;
+    }
+};
+
+//leetcode 
+//valid sudoku
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> seen;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+
+                string row = "row" + to_string(i) + board[i][j];
+                string col = "col" + to_string(j) + board[i][j];
+                string box = "box" + to_string(i / 3) + to_string(j / 3) + board[i][j];
+
+                if (seen.count(row) || seen.count(col) || seen.count(box))
+                    return false;
+
+                seen.insert(row);
+                seen.insert(col);
+                seen.insert(box);
+            }
+        }
+
+        return true;
+    }
+};
